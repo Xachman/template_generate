4fb9bb1e22f636d3fa3e77875ebc7dd0f0f38c68
xachman <zironside@hotmail.com>
2017-12-27 17:53:34 +0000

    get file path from incoming args

--- main.c
+++ main.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "template.h"
 
-int main() {
+char* getFilePath(int argc, char **argv);
 
-    Template tmp = NewTemplate("/test/path");
+int main(int argc, char **argv) {
+    char filepath[4096];
+    strcpy(filepath, getFilePath(argc, argv));
+    Template tmp = NewTemplate(filepath);
 
     printf("%s", tmp.filepath);
 
     return 0;
+}
+
+char* getFilePath(int argc, char **argv) {
+    for( int i=0; i < argc; i++) {
+        if(strcmp(argv[i], "-f") == 0) {
+            if((argc - 1) > i) {
+                return argv[i+1];
+            }
+            printf("%s\n", "No file path in arguments");
+            exit(1);
+        }
+    }
 }
