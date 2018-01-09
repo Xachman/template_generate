8182af64f81754a249b8b2df703b8026f390926a
xach <zironside@hotmail.com>
2018-01-09 09:34:43 -0500

    refactor tests and add new failing case

--- template_test.cpp
+++ template_test.cpp
@@ -8,22 +8,34 @@
 #include "template.h"
 
 TEST_CASE("Test Template Generate", "[templateGenerate]") {
-	std::string subject = "a {{ item }} sentence";
-   
-  	Template tmp = Template(subject);
+	SECTION("one line case with one item") {
+		std::string subject = "a {{ item }} sentence";
+	   
+		Template tmp = Template(subject);
 
-	tmp.setValue("item", "test");
-	
- 	REQUIRE(tmp.generateTemplate().compare("a test sentence") == 0);   
-};
+		tmp.setValue("item", "test");
+		
+		REQUIRE(tmp.generateTemplate().compare("a test sentence") == 0);   
+	}
+	SECTION("One line case with 2 items") {
+		std::string subject = "a {{ item1 }} sentence {{ item2 }} test";
+	   
+		Template tmp = Template(subject);
 
-TEST_CASE("Test Template Generate With 2 items", "[templateGenerate]") {
-	std::string subject = "a {{ item1 }} sentence {{ item2 }} test";
-   
-  	Template tmp = Template(subject);
+		tmp.setValue("item1", "new");
+		tmp.setValue("item2", "to");
+		
+		REQUIRE(tmp.generateTemplate().compare("a new sentence to test") == 0);   
+	}
+	SECTION("Two line case with 3 items") {
+		std::string subject = "a {{ item1 }} sentence {{ item2 }} test\nTest multi line with {{ item3 }}";
+	   
+		Template tmp = Template(subject);
 
-	tmp.setValue("item1", "new");
-	tmp.setValue("item2", "to");
-	
- 	REQUIRE(tmp.generateTemplate().compare("a new sentence to test") == 0);   
+		tmp.setValue("item1", "new");
+		tmp.setValue("item2", "to");
+		tmp.setValue("item3", "new item");
+		
+		REQUIRE(tmp.generateTemplate().compare("a new sentence to test\nTest multi line with new item") == 0);   
+	}
 };
